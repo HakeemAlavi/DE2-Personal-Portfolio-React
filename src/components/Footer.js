8651a6986653911bb863e4d370622b37543db5f5
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-01-26 17:59:55 +0300

    feat: add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer class="bottom-nav-bar">
+        <nav class="bottom-nav">
+            <ul>
+                <li><a href="https://github.com/HakeemAlavi" target="_blank"><img src="media/icons/icons8-github-96.png" alt="GitHub"/></a></li>
+                <li><a href="mailto:abdulhakeemalavi94@gmail.com" target="_blank"><img src="media/icons/icons8-gmail-96.png" alt="Gmail"/></a></li>
+                <li><a href="https://www.linkedin.com/in/hakeem-alavi-8ba632263/" target="_blank"><img src="media/icons/icons8-linkedin-96.png" alt="LinkedIn"/></a></li>
+            </ul>
+        </nav>
+        <div class="copyright">
+            &copy; 2024 Hakeem Alavi All rights reserved
+        </div>
+    </footer>
+    );
+}
+export default Footer;
