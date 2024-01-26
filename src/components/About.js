28eff2687784da0ad799b93562cf8001f87ff833
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-01-26 17:58:52 +0300

    feat: add About component

---
+++ src/components/About.js
@@ -0,0 +1,51 @@
+function About() {
+    return (
+        <section id="about">
+        <div class="about-container">
+            <div class="creative-grid">
+                <div class="creative-grid">
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-university-96.png" alt="University Icon" class="about-icon"/>
+                    </div>
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-kenya-96.png" alt="Nationality Icon" class="about-icon"/>
+                    </div>
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-translate-96.png" alt="Language Icon" class="about-icon"/>
+                    </div>
+
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-passport-94.png" alt="Travel Icon" class="about-icon"/>
+                    </div>
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-ping-pong-94.png" alt="Ping Pong Icon" class="about-icon"/>
+                    </div>
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-books-96.png" alt="Reading Icon" class="about-icon"/>
+                    </div>
+
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-movie-94.png" alt="Movie Icon" class="about-icon"/>
+                    </div>
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-adventure-96.png" alt="Adventure Icon" class="about-icon"/>
+                    </div>
+                    <div class="grid-card">
+                        <img src="media/icons/icons8-video-editing-96.png" alt="Video Editing Icon" class="about-icon"/>
+                    </div>
+                </div>
+            </div>
+            <div class="about-content">
+                <h2>About Me</h2>
+                <p>I'm a Freelancer Front-end Developer with over 2 years of experience. I code and create web elements for amazing people around the world.
+                    <br/><br/>
+                    I pursued a Bachelor in Business and Information Technology in Strathmore University. I'm  a Kenyan based in Nairobi, and I am fluent in English and Swahili, and dabble here and there in French and Urdu.  
+                    <br/><br/>
+                    When I'm not crafting beautiful websites, you will probably find me exploring the world, perfecting my skills on the table tennis court, or hiding in some crevice and lost in a philosophy book, or binging on award winning films, or embracing the spirit of adventure, or creatively expressing myself through video editing.
+                </p>
+            </div>
+        </div>
+    </section>
+    );
+}
+export default About;
