ee0e67096344773ec5d9487fa766dfc027fd15c6
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-01-26 17:58:09 +0300

    feat: add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+function Home() {
+    return (
+        <section id="home">
+        <div class="content">
+            <div class="text">
+                <h2>👋, My name is Hakeem</h2>
+                <h1>I'm a Front-end Developer</h1>
+                <p>I bring designs to life resulting in beautiful websites.</p>
+                <a href="#contact" class="styled-link">HIRE ME</a>
+            </div>
+            <div class="image-container">
+                <img src="media/hakeem-portrait.jpg" alt="Hakeem Alavi" class="self-image"/>
+            </div>
+        </div>
+    </section>
+    );
+}
+export default Home;
