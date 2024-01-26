3b209990a50fc25e613dee52942f56bbb87bab3a
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-01-26 17:57:45 +0300

    feat: add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <div class="header">
+        <div class="branding">
+            <span class="name">Hakeem Alavi</span>
+        </div>
+        <nav class="top-nav">
+            <ul>
+                <li><a href="#contact" class="cta-button">Contact</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#home">Home</a></li>
+            </ul>
+        </nav>
+    </div>
+    );
+}
+export default Header;
