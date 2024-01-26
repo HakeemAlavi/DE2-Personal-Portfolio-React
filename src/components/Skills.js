414d4e64d3c3f383674f8fd3e752f3c623167bcd
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-01-26 17:59:11 +0300

    feat: add Skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,50 @@
+function Skills() {
+    return (
+    <section id="skills">
+        <div class="services-container project-container">
+            <h2>My Projects</h2>
+            
+            <div class="service-card project">
+                <img src="media/mockups/fresh-app.jpg" alt="Project 1 Mockup" class="project-image"/>
+                <div class="project-content">
+                    <h4>Fresh</h4>
+                    <p>Android app for recipe management developed with Kotlin and XML, featuring CRUD capabilities.</p>
+                </div>
+            </div>
+    
+            <div class="service-card project">
+                <img src="media/mockups/warriorguys-game.jpg" alt="Project 2 Mockup" class="project-image"/>
+                <div class="project-content">
+                    <h4>Warrior Guys</h4>
+                    <p>Engaging Unity 3D game with various levels and increasing difficulty, focusing on survival and course completion.</p>
+                </div>
+            </div>
+    
+            <div class="service-card project">
+                <img src="media/mockups/requisition-app.jpg" alt="Project 3 Mockup" class="project-image"/>
+                <div class="project-content">
+                    <h4>Requisition Generator</h4>
+                    <p>Automates company requisition creation using Python and PySimpleGUI for a user-friendly interface.</p>
+                </div>
+            </div>
+    
+            <div class="service-card project">
+                <img src="media/mockups/choleracare-web.jpg" alt="Project 4 Mockup" class="project-image"/>
+                <div class="project-content">
+                    <h4>Cholera Care</h4>
+                    <p>Project involves creating a cholera classification model, implemented as a web app with modern UI/UX, data analytics, and a news web scraper.</p>
+                </div>
+            </div>
+    
+            <div class="service-card project">
+                <img src="media/mockups/toto-clinic-web.jpg" alt="Project 5 Mockup" class="project-image"/>
+                <div class="project-content">
+                    <h4>Toto Clinic</h4>
+                    <p>Website enabling patients to consult a doctor and receive prescriptions, developed with HTML, CSS, PHP, JS, and PhpMyAdmin.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    );
+}
+export default Skills;
