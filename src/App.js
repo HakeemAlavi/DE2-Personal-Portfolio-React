e119f129eb5f05eebf484e8f331a9c7c46847e17
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-01-26 18:00:38 +0300

    feat: add Components to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Services from './components/Services';
+import About from './components/About';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="home">
+      <Header />
+      <Home />
+      <Services />
+      <About />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
