5fd72a6c6e621f194b6ceaeeaf1c832db549ad0c
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-01-26 17:58:26 +0300

    feat: add Services component

---
+++ src/components/Services.js
@@ -0,0 +1,25 @@
+function Services() {
+    return (
+<section id="services">
+        <div class="services-container">
+            <h2>My Services</h2>
+            <div class="service-card">
+                <img src="media/icons/icons8-web-96.png" alt="Web Development Icon" class="service-icon"/>
+                <h4>Web Development</h4>
+                <p>I specialize in creating responsive and user-friendly websites using the latest technologies.</p>
+            </div>
+            <div class="service-card">
+                <img src="media/icons/icons8-design-96.png" alt="Design Icon" class="service-icon"/>
+                <h4>UI/UX Design</h4>
+                <p>Designing visually appealing and intuitive user interfaces to enhance the user experience.</p>
+            </div>
+            <div class="service-card">
+                <img src="media/icons/icons8-frontend-96.png" alt="Frontend Icon" class="service-icon"/>
+                <h4>Front-end Development</h4>
+                <p>Bringing designs to life with clean and efficient front-end code for seamless user interaction.</p>
+            </div>
+        </div>
+    </section>
+    );
+}
+export default Services;
